508080947cadac1b50c35697103de2fc57af0395
timonson <timo.rossa@tutanota.com>
2021-01-26 15:04:14 +0100

    Add headers last-modified and content-type and make the middleware async

--- middlewares/pressf-static/mod.ts
+++ middlewares/pressf-static/mod.ts
@@ -1,5 +1,6 @@
 import { Context, parse } from "../../pressf.ts";
 import { join } from "https://deno.land/std@0.80.0/path/mod.ts";
+import { lookup } from "https://deno.land/x/media_types@v2.7.1/mod.ts";
 
 /**
 * A minimal static file server middleware.
@@ -7,14 +8,28 @@ import { join } from "https://deno.land/std@0.80.0/path/mod.ts";
 export default (prefix: string, root: string) => {
   const routePattern = parse(`${prefix}/*`).pattern;
 
-  return (ctx: Context) => {
-    let matches = routePattern.exec(ctx.url);
+  return async (ctx: Context) => {
+    const matches = routePattern.exec(ctx.url);
     if (
       matches && matches.length == 2 &&
       (ctx.method === "HEAD" || ctx.method === "GET")
     ) {
-      let r = Deno.readFileSync(join(root, matches[1]));
-      ctx.respond({ body: r });
+      try {
+        const path = join(root, matches[1]);
+        const r = await Deno.readFile(path);
+        const info = await Deno.stat(path);
+        const headers = new Headers();
+        const contentType = lookup(path);
+        if (contentType) {
+          headers.set("content-type", contentType);
+        }
+        if (info.mtime) {
+          headers.set("last-modified", info.mtime.toUTCString());
+        }
+        ctx.respond({ body: r, headers });
+      } catch {
+        ctx.respond({ status: 404 });
+      }
     }
   };
 };
