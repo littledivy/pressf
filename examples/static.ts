3f51b116b6fd6af5887a3fe8ee454a7868c2f55a
timonson <timo.rossa@tutanota.com>
2021-01-27 19:39:42 +0100

    Add an optional home file

--- examples/static.ts
+++ examples/static.ts
@@ -2,6 +2,6 @@ import PressF from "../pressf.ts";
 import serveStatic from "../middlewares/pressf-static/mod.ts";
 
 const ctx = new PressF();
-ctx.use(serveStatic("static", "./examples"));
+ctx.use(serveStatic("./examples/static"));
 
 await ctx.listen(8080);
