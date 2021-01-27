fcd63b3c335f55be26f0eda58802637014dbfa1f
timonson <timo.rossa@tutanota.com>
2021-01-27 19:38:25 +0100

    Fix parse by removing a leading slash from string and remove Boolean type from keys; change All type from empty string to ALL

--- pressf.ts
+++ pressf.ts
@@ -3,30 +3,30 @@ import { serve, ServerRequest } from "https://deno.land/std/http/server.ts";
 type Params = { [key: string]: string };
 
 // Common pre-parsed routes
-const rootParse = { keys: false, pattern: /^\/\/\/?$/i };
+const rootParse = { keys: [], pattern: /^\/\/\/?$/i };
 const wildParse = { keys: ["wild"], pattern: /^\/(.*)\/?$/i };
 
 // Adapted from https://github.com/lukeed/regexparam/blob/master/src/index.js
 export function parse(
   str: RegExp | string,
   loose?: boolean,
-): { keys: string[] | boolean; pattern: RegExp } {
-  if (str instanceof RegExp) return { keys: false, pattern: str };
+): { keys: string[]; pattern: RegExp } {
+  if (str instanceof RegExp) return { keys: [], pattern: str };
   if (str === "/") return rootParse;
   else if (str === "*") return wildParse;
-  var arr = str.split("/");
-  var len = arr.length;
-  var i = 0, c, keys = [], pattern = "";
-  while (i < len) {
-    var t = arr[i];
-    c = t[0];
-    i++;
+  const arr = str[0] === "/" ? str.slice(1).split("/") : str.split("/");
+  const keys = [];
+  const len = arr.length;
+  let pattern = "";
+  for (let i = 0; i < len; i++) {
+    const t = arr[i];
+    const c = t[0];
     if (c === "*") {
       keys.push("wild");
       pattern += "/(.*)";
     } else if (c === ":") {
-      var o = t.indexOf("?", 1);
-      var ext = t.indexOf(".", 1);
+      const o = t.indexOf("?", 1);
+      const ext = t.indexOf(".", 1);
       // Double negation turn out to be faster than Boolean() casts
       // deno-lint-ignore no-extra-boolean-cast
       keys.push(t.substring(1, !!~o ? o : !!~ext ? ext : t.length));
@@ -47,9 +47,8 @@ export function parse(
 type RouteFn = (ctx: Context) => void;
 type RoutePattern = RegExp;
 export type Context = ServerRequest & { params: Params };
-// XXX(TODO): Use ALL instead or ""?
 type Method =
-  | ""
+  | "ALL"
   | "GET"
   | "POST"
   | "HEAD"
@@ -65,16 +64,15 @@ type Method =
 type Route = {
   pattern?: RoutePattern;
   method: Method;
-  // XXX: Why not null?
-  keys: boolean | string[];
+  keys: string[];
   handlers: RouteFn[];
 };
+
 export default class Router {
-  // NOTE: This is transpiled into the constructor, therefore equivalent to this.routes = [];
   routes: Route[] = [];
 
   // NOTE: Using .bind can significantly increase perf compared to arrow functions.
-  public all = this.add.bind(this, "");
+  public all = this.add.bind(this, "ALL");
   public get = this.add.bind(this, "GET");
   public head = this.add.bind(this, "HEAD");
   public patch = this.add.bind(this, "PATCH");
@@ -85,8 +83,9 @@ export default class Router {
   public post = this.add.bind(this, "POST");
   public put = this.add.bind(this, "PUT");
 
+  // Applies the handlers to all methods and urls
   public use(...handlers: RouteFn[]) {
-    this.routes.push({ keys: false, method: "", handlers });
+    this.routes.push({ keys: [], method: "ALL", handlers });
     return this;
   }
 
@@ -100,20 +99,18 @@ export default class Router {
     const server = serve({ port });
     for await (const req of server) {
       if (this.routes.length > 0) {
-        var i = 0,
-          len = this.routes.length;
-        while (i < len) {
-          var r = this.routes[i];
-          i++;
-          var params: Params = {};
+        const len = this.routes.length;
+        for (let i = 0; i < len; i++) {
+          const r = this.routes[i];
+          const params: Params = {};
           // NOTE: We're caching length here.
-          var keyLength = typeof r.keys !== "boolean" ? r.keys.length : 0;
+          const keyLength = r.keys.length;
           if (keyLength > 0 && r.pattern) {
-            var matches = r.pattern.exec(req.url);
-            // XXX(BRUH): Typescript is not really cooperating here, we've already added checks for r.keys being boolean.
-            if (matches && typeof r.keys !== "boolean") {
-              var inc = 0;
-              while (inc < keyLength) params[r.keys[inc]] = matches[++inc];
+            const matches = r.pattern.exec(req.url);
+            if (matches) {
+              for (let inc = 0; inc < keyLength; inc++) {
+                params[r.keys[inc]] = matches[inc];
+              }
             }
           }
           if (!r.pattern) {
