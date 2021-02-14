5de7c9197e153c71fd03b35f16bb9414f8722488
timonson <timo.rossa@tutanota.com>
2021-02-14 12:44:03 +0100

    Update examples

--- examples/logger.ts
+++ examples/logger.ts
@@ -1,10 +1,12 @@
 import PressF from "../pressf.ts";
 import logger from "../middlewares/pressf-logger/mod.ts";
 import serveStatic from "../middlewares/pressf-static/mod.ts";
+import errorHandler from "../middlewares/pressf-error-handler/mod.ts";
 
 const ctx = new PressF();
 
 ctx.use(logger());
 ctx.use(serveStatic("./examples/static"));
+ctx.use(errorHandler);
 
 await ctx.listen(8080);
--- examples/static.ts
+++ examples/static.ts
@@ -1,7 +1,10 @@
-import PressF from "../pressf.ts";
+import PressF, { Context } from "../pressf.ts";
 import serveStatic from "../middlewares/pressf-static/mod.ts";
+import errorHandler from "../middlewares/pressf-error-handler/mod.ts";
 
 const ctx = new PressF();
+
 ctx.use(serveStatic(new URL("./static", import.meta.url).pathname));
+ctx.use(errorHandler);
 
 await ctx.listen(8080);
