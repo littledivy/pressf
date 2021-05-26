b549dc6be88a53a5c56f6341c1e7954369d0898b
timonson <timo.rossa@tutanota.com>
2021-05-26 06:09:35 +0200

    Bump versions

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,6 +1,6 @@
-export { ServerRequest } from "https://deno.land/std@0.88.0/http/server.ts";
+export { ServerRequest } from "https://deno.land/std@0.97.0/http/server.ts";
 export {
   assertEquals,
   assertThrowsAsync,
-} from "https://deno.land/std@0.88.0/testing/asserts.ts";
-export { delay } from "https://deno.land/std@0.88.0/async/delay.ts";
+} from "https://deno.land/std@0.97.0/testing/asserts.ts";
+export { delay } from "https://deno.land/std@0.97.0/async/delay.ts";
